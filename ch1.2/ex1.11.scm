05ece3139208d5340d67bd022816cc0cf0ec76cd
Marnen Laibow-Koser <marnen@marnen.org>
2014-11-25 01:08:37 +0000

    Iterative implementation, after http://www.billthelizard.com/2009/11/sicp-exercise-111.html [1.11]

--- ch1.2/ex1.11.scm
+++ ch1.2/ex1.11.scm
@@ -1,7 +1,12 @@
 (define (f n)
+  (define (f-iter a b c n)
+    (if (< n 3)
+        a
+        (f-iter
+          (+ a (* 2 b) (* 3 c))
+          a
+          b
+          (- n 1))))
   (if (< n 3)
-    n
-    (+
-      (f (- n 1))
-      (* 2 (f (- n 2)))
-      (* 3 (f (- n 3))))))
+      n
+      (f-iter 2 1 0 n)))
