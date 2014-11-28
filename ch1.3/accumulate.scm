5755d194ff07e5d298e52019132eb658abac366d
Marnen Laibow-Koser <marnen@marnen.org>
2014-11-28 22:00:15 +0000

    Recursive implementation of product. [1.31]

--- ch1.3/accumulate.scm
+++ ch1.3/accumulate.scm
@@ -12,6 +12,11 @@
         (iter (next a) (* (term a) result))))
   (iter a 1))
   
+(define (product-recursive term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product-recursive term (next a) next b))))
+  
 (define (factorial n)
   (define (id n) n)
   (product id 1 1+ n))
