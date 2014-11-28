5eefbe1b5229663b2cf3de65d9de29b5afed718f
Marnen Laibow-Koser <marnen@marnen.org>
2014-11-28 21:50:59 +0000

    Add product function. [1.31]

--- ch1.3/accumulate.scm
+++ ch1.3/accumulate.scm
@@ -4,3 +4,10 @@
         result
         (iter (next a) (+ (term a) result))))
   (iter a 0))
+  
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
