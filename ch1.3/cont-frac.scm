8ed0d37b2e5dc10f888e7daa34e34183f7dfbea6
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-23 21:58:49 +0000

    Lambert's formula for tangent. [1.39]

--- ch1.3/cont-frac.scm
+++ ch1.3/cont-frac.scm
@@ -17,3 +17,10 @@
                         (* 2 (ceiling (/ i 3)))
                         1))
                   iterations)))
+                  
+(define (tan-cf x iterations)
+  (cont-frac (lambda (i)
+               (if (= i 1) x (- (* x x))))
+             (lambda (i)
+               (- (* 2 i) 1))
+             iterations))
