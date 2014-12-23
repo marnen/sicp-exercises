9893b38dae32a577cab3d25ea4defc5bbd9dd7c7
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-23 22:14:42 +0000

    Add Newton's method from book. [1.40]

--- ch1.3/fixed-point.scm
+++ ch1.3/fixed-point.scm
@@ -12,3 +12,18 @@
           next
           (try next))))
   (try first-guess))
+  
+(define (deriv g)
+  (let ((dx 0.00001))
+    (lambda (x)
+      (/ (- (g (+ x dx)) (g x))
+         dx))))
+       
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) 
+            ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) 
+               guess))
