713d592f7451c57add4865c86a3233aee46c093f
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-23 20:10:37 +0000

    Rewrite iteratively. [1.37]

--- ch1.3/cont-frac.scm
+++ ch1.3/cont-frac.scm
@@ -1,5 +1,11 @@
 (define (cont-frac numerator denominator count)
-  (if (= count 0)
-      0
-      (/ (numerator count)
-         (+ (denominator count) (cont-frac numerator denominator (- count 1))))))
+  (define (cont-frac-iter numerator denominator count a)
+    (if (= count 0)
+        a
+        (cont-frac-iter
+          numerator
+          denominator
+          (- count 1)
+          (/ (numerator count)
+             (+ (denominator count) a)))))
+  (cont-frac-iter numerator denominator count 0))
