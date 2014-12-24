7cb17524472f62c03316d769efa3abfe2a6f7115
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-24 07:56:07 +0000

    Basic constructors and selectors. [2.2]

--- ch2.1/segment.scm
+++ ch2.1/segment.scm
@@ -1,3 +1,21 @@
+(define (make-segment start end)
+  (cons start end))
+  
+(define (start-segment segment)
+  (car segment))
+  
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+  
+(define (x-point p)
+  (car p))
+  
+(define (y-point p)
+  (cdr p))
+
 (define (print-point p)
   (newline)
   (display "(")
