475d4b165e4daa88c85f66f191b0567d374dd0c7
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-24 22:41:34 +0000

    Refactor. [2.5]

--- ch2.1/prime-pair.scm
+++ ch2.1/prime-pair.scm
@@ -6,12 +6,13 @@
 (define (cons a b)
   (* (power 2 a) (power 3 b)))
   
-(define (car pair)
-  (if (zero? (mod pair 2))
-    (+ 1 (car (/ pair 2)))
+(define (factor-power n factor)
+  (if (zero? (mod n factor))
+    (+ 1 (factor-power (/ n factor) factor))
     0))
+  
+(define (car pair)
+  (factor-power pair 2))
     
 (define (cdr pair)
-  (if (zero? (mod pair 3))
-    (+ 1 (cdr (/ pair 3)))
-    0))
+  (factor-power pair 3))
