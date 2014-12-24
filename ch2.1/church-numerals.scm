a5e9c05027506f9455fd7627e66ef2cff9339aa2
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-24 23:35:45 +0000

    Write Church 1 as a function. [2.6]

--- ch2.1/church-numerals.scm
+++ ch2.1/church-numerals.scm
@@ -1,18 +1,6 @@
 (define zero (lambda (f) (lambda (x) x)))
 
+(define one (lambda (f) (lambda (x) (f x))))
+
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
-  
-; derivation of (one) from this 
-  
-(add-1 zero)
-
-(lambda (f)
-  (lambda (x)
-    (f (((lambda (f) (lambda (x) x)) f) x))))
-
-(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
-
-(lambda (f) (lambda (x) (f x)))
-
-(lambda (f) (lambda (x) (f x)))
