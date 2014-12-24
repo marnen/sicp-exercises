73bebf3faad0c3a272708d13451cc5571eb409d9
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-24 23:23:54 +0000

    Derive Church numeral 1. [2.6]

--- ch2.1/church-numerals.scm
+++ ch2.1/church-numerals.scm
@@ -2,3 +2,17 @@
 
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
+  
+; derivation of (one) from this 
+  
+(add-1 zero)
+
+(lambda (f)
+  (lambda (x)
+    (f (((lambda (f) (lambda (x) x)) f) x))))
+
+(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+
+(lambda (f) (lambda (x) (f x)))
+
+(lambda (f) (lambda (x) (f x)))
