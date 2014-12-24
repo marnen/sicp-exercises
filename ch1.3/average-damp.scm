38be79f6d9ad90d6412cced2c6bce11798782b33
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-24 06:54:35 +0000

    Implement average-damped nth root. [1.45]

--- ch1.3/average-damp.scm
+++ ch1.3/average-damp.scm
@@ -1,3 +1,18 @@
 (define (average-damp f)
   (lambda (x)
     (/ (+ x (f x)) 2)))
+    
+(load "fixed-point")
+(load "compose")
+
+(define (power x y)
+  (if (= y 0)
+    1
+    (* x (power x (- y 1)))))
+
+(define (nth-root x n)
+  (let ((damp-count (max (- n 2) 1)))
+    (fixed-point
+      ((repeated average-damp damp-count)
+        (lambda (y) (/ x (power y (- n 1)))))
+      1)))
