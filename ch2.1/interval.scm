ff2b90113291a127847d556c8dd0d0dcd41d25a7
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 02:01:25 +0000

    Remove ordering. [2.7][2.8]

--- ch2.1/interval.scm
+++ ch2.1/interval.scm
@@ -1,10 +1,10 @@
 (define (make-interval a b) (cons a b))
 
 (define (upper-bound interval)
-  (max (car interval) (cdr interval)))
+  (cdr interval))
   
 (define (lower-bound interval)
-  (min (car interval) (cdr interval)))
+  (car interval))
 
 (define (add-interval x y)
   (make-interval (+ (lower-bound x) 
