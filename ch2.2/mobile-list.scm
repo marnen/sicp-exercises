00ddaa533988e15b9c69b81fcc050fa8a79d2468
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 19:09:31 +0000

    Write selectors. [2.29]

--- ch2.2/mobile-list.scm
+++ ch2.2/mobile-list.scm
@@ -3,3 +3,15 @@
 
 (define (make-branch length structure)
   (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cadr branch))
