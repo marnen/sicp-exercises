6deb808447393180f68342b6edbae84714dd004d
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 01:56:33 +0000

    Define upper-bound and lower-bound. [2.7]

--- ch2.1/interval.scm
+++ ch2.1/interval.scm
@@ -1,5 +1,11 @@
 (define (make-interval a b) (cons a b))
 
+(define (upper-bound interval)
+  (max (car interval) (cdr interval)))
+  
+(define (lower-bound interval)
+  (min (car interval) (cdr interval)))
+
 (define (add-interval x y)
   (make-interval (+ (lower-bound x) 
                     (lower-bound y))
