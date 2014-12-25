65dd12366ac68db91d6ddef4487179d47f5ec194
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 00:21:54 +0000

    Simulate run of add-church. [2.6]

--- ch2.1/church-numerals.scm
+++ ch2.1/church-numerals.scm
@@ -6,36 +6,33 @@
 
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
+  
+(define (add-church m n)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
 
-; derive add-2
+; expand add-church to make sure it works
 
-(add-1 (add-1 n))
+; (add-church one two)
 
-(lambda (f)
-  (lambda (x)
-    (f (((lambda (f) (lambda (x) (f ((n f) x)))) f) x))))
+; (lambda (f) (lambda (x) ((one f) ((two f) x))))
 
-(lambda (f) (lambda (x) (f (f ((n f) x)))))
+; (lambda (f)
+;   (lambda (x)
+;     ((one f)
+;     (((lambda (f) (lambda (x) (f (f x)))) f) x))))
+     
+; (lambda (f)
+;   (lambda (x)
+;     ((one f)
+;     ((lambda (x) (f (f x))) x))))
 
-; derive add-0
+; (lambda (f) (lambda (x) ((one f) (f (f x)))))
 
-((lambda (n) (lambda (f) (lambda (x) ((n f) x)))) one)
+; (lambda (f)
+;   (lambda (x)
+;     (((lambda (f) (lambda (x) (f x))) f) (f (f x)))))
+    
+; (lambda (f)
+;   (lambda (x) ((lambda (x) (f x)) (f (f x)))))
 
-(lambda (f) (lambda (x) ((one f) x)))
-
-(lambda (f) (lambda (x) ((lambda (x) (f x)) x)))
-
-(lambda (f) (lambda (x) (f x)))
-
-; make general add-m (such that (add-m 1) = (add-1)
-((lambda (m) (lambda (f) (lambda (x) ((m f) ((n f) x))))) one)
-
-(lambda (f) (lambda (x) ((one f) ((n f) x))))
-
-(lambda (f)
-  (lambda (x)
-    ((lambda (x) (f x)) ((n f) x))))
-
-(lambda (f)
-  (lambda (x)
-    (f ((n f) x))))
+; (lambda (f) (lambda (x) (f (f (f x)))))
