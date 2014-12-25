eb03cc68e3d8e83aefec6bbb5033c0ed46baf81f
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 19:48:11 +0000

    Map implementation of square-tree. [2.30]

--- ch2.2/square-tree.scm
+++ ch2.2/square-tree.scm
@@ -1,7 +1,15 @@
+(define (square n) (* n n))
+
 (define (square-tree-direct tree)
-  (define (square n) (* n n))
   (cond ((null? tree) tree)
         ((not (pair? tree)) (square tree))
         (else
           (cons (square-tree-direct (car tree))
                 (square-tree-direct (cdr tree))))))
+
+(define (square-tree-map tree)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+             (square-tree-map subtree)
+             (square subtree)))
+       tree))
