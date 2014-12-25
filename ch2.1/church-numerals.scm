237de31bc4962e1537c68de3a7421dfc35fdf73a
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-25 00:12:19 +0000

    Derive generalized addition function. [2.6]

--- ch2.1/church-numerals.scm
+++ ch2.1/church-numerals.scm
@@ -16,3 +16,26 @@
     (f (((lambda (f) (lambda (x) (f ((n f) x)))) f) x))))
 
 (lambda (f) (lambda (x) (f (f ((n f) x)))))
+
+; derive add-0
+
+((lambda (n) (lambda (f) (lambda (x) ((n f) x)))) one)
+
+(lambda (f) (lambda (x) ((one f) x)))
+
+(lambda (f) (lambda (x) ((lambda (x) (f x)) x)))
+
+(lambda (f) (lambda (x) (f x)))
+
+; make general add-m (such that (add-m 1) = (add-1)
+((lambda (m) (lambda (f) (lambda (x) ((m f) ((n f) x))))) one)
+
+(lambda (f) (lambda (x) ((one f) ((n f) x))))
+
+(lambda (f)
+  (lambda (x)
+    ((lambda (x) (f x)) ((n f) x))))
+
+(lambda (f)
+  (lambda (x)
+    (f ((n f) x))))
