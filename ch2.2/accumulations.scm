a94690c3d7ff29afbd3901dea812fc204224b44d
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-28 23:47:44 +0000

    Implement map. [2.33]

--- ch2.2/accumulations.scm
+++ ch2.2/accumulations.scm
@@ -5,3 +5,7 @@
           (accumulate op
                       initial
                       (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y))
+              () sequence))
