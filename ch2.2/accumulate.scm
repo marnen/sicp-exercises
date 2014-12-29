0d00612a2c708b2d80f3f70b7db891e204be0da0
Marnen Laibow-Koser <marnen@marnen.org>
2014-12-29 00:04:45 +0000

    Horner skeleton from book. [2.34]

--- ch2.2/accumulate.scm
+++ ch2.2/accumulate.scm
@@ -5,13 +5,3 @@
           (accumulate op
                       initial
                       (cdr sequence)))))
-
-(define (map p sequence)
-  (accumulate (lambda (x y) (cons (p x) y))
-              () sequence))
-
-(define (append seq1 seq2)
-  (accumulate cons seq2 seq1))
-
-(define (length sequence)
-  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
